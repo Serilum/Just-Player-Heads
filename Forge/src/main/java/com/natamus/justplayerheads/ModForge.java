882c17448d767bc04e2eab66668103d6d5209672
Rick South <rick@natamus.com>
2023-12-12 09:47:28 +0100

    Just Player Heads source code for 1.20.0.

---
+++ Forge/src/main/java/com/natamus/justplayerheads/ModForge.java
@@ -0,0 +1,36 @@
+package com.natamus.justplayerheads;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.justplayerheads.forge.config.IntegrateForgeConfig;
+import com.natamus.justplayerheads.forge.events.ForgePlayerHeadEvent;
+import com.natamus.justplayerheads.util.Reference;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+    	MinecraftForge.EVENT_BUS.register(new ForgePlayerHeadEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}

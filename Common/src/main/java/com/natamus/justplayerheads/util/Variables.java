a24fae253bb1b796947f59ca2e6c83403d1a64a0
Rick South <rick@natamus.com>
2023-12-12 09:47:07 +0100

    Just Player Heads source code for 1.18.2.

---
+++ Common/src/main/java/com/natamus/justplayerheads/util/Variables.java
@@ -0,0 +1,9 @@
+package com.natamus.justplayerheads.util;
+
+import java.util.HashMap;
+
+import net.minecraft.world.item.ItemStack;
+
+public class Variables {
+	public static HashMap<String, ItemStack> headcache = new HashMap<String, ItemStack>();
+}

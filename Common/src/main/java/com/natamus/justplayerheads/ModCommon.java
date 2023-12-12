10c32b385f1fef0da770f0d68280e5719082c7f3
Rick South <rick@natamus.com>
2023-12-12 09:47:38 +0100

    Just Player Heads source code for 1.20.2.

---
+++ Common/src/main/java/com/natamus/justplayerheads/ModCommon.java
@@ -0,0 +1,15 @@
+package com.natamus.justplayerheads;
+
+import com.natamus.justplayerheads.config.ConfigHandler;
+
+public class ModCommon {
+
+	public static void init() {
+		ConfigHandler.initConfig();
+		load();
+	}
+
+	private static void load() {
+		
+	}
+}

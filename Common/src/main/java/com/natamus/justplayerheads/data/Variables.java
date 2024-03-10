399f0355f239e313bc232ef4bd102997e35e9a37
Rick South <rick@natamus.com>
2024-03-10 16:34:10 +0100

    (v3.6) Updated mod to work with the new player head caching feature in Collective. [+1]

--- Common/src/main/java/com/natamus/justplayerheads/data/Variables.java
+++
@@ -1,9 +0,0 @@
-package com.natamus.justplayerheads.data;
-
-import net.minecraft.world.item.ItemStack;
-
-import java.util.HashMap;
-
-public class Variables {
-	public static HashMap<String, ItemStack> headcache = new HashMap<String, ItemStack>();
-}

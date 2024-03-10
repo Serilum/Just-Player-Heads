a7cc705d3bd508d69e072ba766904f69779bc3b1
Rick South <rick@natamus.com>
2024-03-10 16:34:19 +0100

    (v3.6) Updated mod to work with the new player head caching feature in Collective. [+1]

--- Common/src/main/java/com/natamus/justplayerheads/cmds/CommandJph.java
+++ Common/src/main/java/com/natamus/justplayerheads/cmds/CommandJph.java
@@ -4,18 +4,14 @@ import com.mojang.brigadier.CommandDispatcher;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.arguments.StringArgumentType;
 import com.mojang.brigadier.context.CommandContext;
-import com.natamus.collective.functions.HeadFunctions;
+import com.natamus.collective.features.PlayerHeadCacheFeature;
 import com.natamus.collective.functions.ItemFunctions;
 import com.natamus.collective.functions.MessageFunctions;
-import com.natamus.justplayerheads.config.ConfigHandler;
-import com.natamus.justplayerheads.data.Variables;
 import net.minecraft.ChatFormatting;
 import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-
-import java.util.HashMap;
 
 public class CommandJph {
 	public static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
@@ -42,16 +38,6 @@ public class CommandJph {
 					return 1;
 				})))
 
-				.then(Commands.literal("cache")
-				.executes((command) -> {
-					CommandSourceStack source = command.getSource();
-					
-					Variables.headcache = new HashMap<String, ItemStack>();
-					
-					MessageFunctions.sendMessage(source, "The player head texture cache has been emptied.", ChatFormatting.DARK_GREEN);
-					return 1;
-				}))
-
 				.executes((command) -> {
 					CommandSourceStack source = command.getSource();
 
@@ -64,38 +50,22 @@ public class CommandJph {
 	
 	public static void processJph(CommandContext<CommandSourceStack> command, Integer amount) {
 		CommandSourceStack source = command.getSource();
-		
-		String target = StringArgumentType.getString(command, "name");
-		
-		ItemStack head = null;
-		if (ConfigHandler.enablePlayerHeadCaching) {
-			if (Variables.headcache.containsKey(target.toLowerCase())) {
-				head = Variables.headcache.get(target.toLowerCase());
-				head.setCount(amount);
-			}
-		}
-		
-		if (head == null) {
-			head = HeadFunctions.getPlayerHead(target, amount);
-			
-			if (head != null && ConfigHandler.enablePlayerHeadCaching) {
-				ItemStack cachehead = head.copy();
-				cachehead.setCount(1);
-				
-				Variables.headcache.put(target.toLowerCase(), cachehead);
-			}
-		}
 
-		if (head == null) {
-			String message = "Unable to generate the player head. Either the player '" + target + "' does not exist or the Mojang API server has had too many requests in a short period of time.";
-			MessageFunctions.sendMessage(source, message, ChatFormatting.DARK_RED);
-		}
-		else {
+		String target = StringArgumentType.getString(command, "name");
+
+		ItemStack headStack = PlayerHeadCacheFeature.getPlayerHeadStackFromCache(target);
+
+		if (headStack != null) {
+			headStack.setCount(amount);
+
 			String message = "Succesfully generated the head of the player '" + target + "'.";
 			MessageFunctions.sendMessage(source, message, ChatFormatting.DARK_GREEN);
-			
-			Player player = source.getPlayer();
-			ItemFunctions.giveOrDropItemStack(player, head);
+
+			ItemFunctions.giveOrDropItemStack(source.getPlayer(), headStack);
+		}
+		else {
+			String message = "Unable to generate the player head. Either the player '" + target + "' does not exist or the Mojang API server has had too many requests in a short period of time.";
+			MessageFunctions.sendMessage(source, message, ChatFormatting.RED);
 		}
 	}
 }

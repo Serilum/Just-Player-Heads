4a49d5786c019e105d945f668d4d751a54b08ecb
Rick South <rick@natamus.com>
2024-04-21 21:42:41 +0200

    (v4.0) Updated Just Player Heads to use the improved Collective code. [+2]

--- Common/src/main/java/com/natamus/justplayerheads/config/ConfigHandler.java
+++ Common/src/main/java/com/natamus/justplayerheads/config/ConfigHandler.java
@@ -10,14 +10,10 @@ import java.util.List;
 public class ConfigHandler extends DuskConfig {
 	public static HashMap<String, List<String>> configMetaData = new HashMap<String, List<String>>();
 
-	@Entry public static boolean enablePlayerHeadCaching = true;
 	@Entry public static boolean playerDropsHeadOnDeath = true;
 	@Entry(min = 0.0001, max = 1.0) public static double playerHeadDropChance = 1.0;
 
 	public static void initConfig() {
-		configMetaData.put("enablePlayerHeadCaching", Arrays.asList(
-			"If enabled, enables the caching of player head textures. This fixes the limitations of the Mojang API, but does mean that whenever players update their skin the old head is still dropped until the server reboots or an administator uses the '/jph cache' command."
-		));
 		configMetaData.put("playerDropsHeadOnDeath", Arrays.asList(
 			"If enabled, allows players to drop their head on death."
 		));

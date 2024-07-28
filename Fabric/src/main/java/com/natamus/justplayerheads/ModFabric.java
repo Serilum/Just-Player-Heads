4ca581191edd591dd2b87d2ef459efe939d0811d
Rick South <rick@natamus.com>
2024-07-28 07:26:57 +0200

    (v4.1) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Fabric/src/main/java/com/natamus/justplayerheads/ModFabric.java
+++ Fabric/src/main/java/com/natamus/justplayerheads/ModFabric.java
@@ -1,6 +1,7 @@
 package com.natamus.justplayerheads;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.collective.fabric.callbacks.CollectivePlayerEvents;
 import com.natamus.justplayerheads.cmds.CommandJph;
 import com.natamus.justplayerheads.events.PlayerHeadEvent;
@@ -14,6 +15,10 @@ public class ModFabric implements ModInitializer {
 	
 	@Override
 	public void onInitialize() {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		setGlobalConstants();
 		ModCommon.init();
 
--- NeoForge/src/main/java/com/natamus/justplayerheads/ModNeoForge.java
+++ NeoForge/src/main/java/com/natamus/justplayerheads/ModNeoForge.java
@@ -1,6 +1,7 @@
 package com.natamus.justplayerheads;
 
 import com.natamus.collective.check.RegisterMod;
+import com.natamus.collective.check.ShouldLoadCheck;
 import com.natamus.justplayerheads.neoforge.config.IntegrateNeoForgeConfig;
 import com.natamus.justplayerheads.neoforge.events.NeoForgePlayerHeadEvent;
 import com.natamus.justplayerheads.util.Reference;
@@ -15,6 +16,10 @@ import net.neoforged.fml.javafmlmod.FMLJavaModLoadingContext;
 public class ModNeoForge {
 	
 	public ModNeoForge(IEventBus modEventBus) {
+		if (!ShouldLoadCheck.shouldLoad(Reference.MOD_ID)) {
+			return;
+		}
+
 		modEventBus.addListener(this::loadComplete);
 
 		setGlobalConstants();

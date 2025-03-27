ad8a7e2559a07e443c6cb924472bdca4db730eba
Rick South <rick@natamus.com>
2025-03-28 00:43:05 +0100

    Just Player Heads source code for 1.21.5.

---
+++ Forge/src/main/java/com/natamus/justplayerheads/forge/events/ForgePlayerHeadEvent.java
@@ -0,0 +1,34 @@
+package com.natamus.justplayerheads.forge.events;
+
+import com.natamus.justplayerheads.cmds.CommandJph;
+import com.natamus.justplayerheads.events.PlayerHeadEvent;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+public class ForgePlayerHeadEvent {
+    @SubscribeEvent
+    public static void registerCommands(RegisterCommandsEvent e) {
+        CommandJph.register(e.getDispatcher());
+    }
+	
+	@SubscribeEvent
+	public static void entityDeath(LivingDeathEvent e) {
+		Entity entity = e.getEntity();
+		Level level = entity.level();
+        if (level.isClientSide) {
+            return;
+        }
+
+        if (!(entity instanceof Player)) {
+            return;
+        }
+
+        PlayerHeadEvent.onPlayerDeath((ServerLevel)level, (ServerPlayer)entity);
+	}
+}

927e1ee86c894611a2d8e9e0f3bd4b4303a72af0
Rick South <rick@natamus.com>
2025-06-19 03:47:59 +0200

    Just Player Heads source code for 1.21.6.

---
+++ Common/src/main/java/com/natamus/justplayerheads/config/ConfigHandler.java
@@ -0,0 +1,26 @@
+package com.natamus.justplayerheads.config;
+
+import com.natamus.collective.config.DuskConfig;
+import com.natamus.justplayerheads.util.Reference;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class ConfigHandler extends DuskConfig {
+	public static HashMap<String, List<String>> configMetaData = new HashMap<String, List<String>>();
+
+	@Entry public static boolean playerDropsHeadOnDeath = true;
+	@Entry(min = 0.0001, max = 1.0) public static double playerHeadDropChance = 1.0;
+
+	public static void initConfig() {
+		configMetaData.put("playerDropsHeadOnDeath", Arrays.asList(
+			"If enabled, allows players to drop their head on death."
+		));
+		configMetaData.put("playerHeadDropChance", Arrays.asList(
+			"Sets the chance of a player dropping their head if 'playerDropsHeadOnDeath' is enabled."
+		));
+
+		DuskConfig.init(Reference.NAME, Reference.MOD_ID, ConfigHandler.class);
+	}
+}
